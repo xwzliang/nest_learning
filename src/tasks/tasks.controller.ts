1e33f69d8e6c4035a4cd99fe2c6b0c8505b8be34
broliang <18706703437@163.com>
2020-09-15 17:41:09 +0800

    Add src/tasks/tasks.controller.ts: Add createTask handler to TasksController.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
@@ -10,4 +10,18 @@ export class TasksController {
     getAllTasks():Task[] {
         return this.tasksService.getAllTasks();
     }
+
+    @Post()
+    // Retrive specific parameter from request body
+    createTask(
+        @Body('title') title: string,
+        @Body('description') description: string,
+    ): Task{
+        return this.tasksService.createTask(title, description);
+    }
+
+    // Retrive all parameters from request body
+    // createTask(@Body() body) {
+    //     console.log('body', body);
+    // }
 }
